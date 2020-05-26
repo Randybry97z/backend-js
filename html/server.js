635099613a9aa3472cd7d968fa28ba770c3b383d
Bryan <bssandovalrod@gmail.com>
2020-05-26 14:54:52 -0500

    Define view engine

--- html/server.js
+++ html/server.js
@@ -2,13 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine','ejs')
+
 app.use('/assets',express.static('assets'))
 
 app.get('/', function (req,res) {
-	res.sendFile("index.html",{
-		root: __dirname
-	})
-	//res.send(__dirname)
+	res.render('index')
 })
 
 app.listen(3000)
