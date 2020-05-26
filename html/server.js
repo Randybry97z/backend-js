8886d7995a1c946b0349eee9f3118316a9bc1b44
Bryan <bssandovalrod@gmail.com>
2020-05-26 15:08:47 -0500

    Manipulating cache

--- html/server.js
+++ html/server.js
@@ -4,7 +4,10 @@ const app = express()
 
 app.set('view engine','ejs')
 
-app.use('/assets',express.static('assets'))
+app.use('/public',express.static('assets',{
+	etag: false,
+	maxAge: '5h' //Tiempo de vigencia de cache
+})) //middleware
 
 app.get('/', function (req,res) {
 	res.render('index')
