6328a951b9bb92f3272022e2adaf15bc147949a7
Bryan <bssandovalrod@gmail.com>
2020-05-26 14:49:33 -0500

    adding assets folder to the project

--- html/server.js
+++ html/server.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.use('/assets',express.static('assets'))
+
 app.get('/', function (req,res) {
-	//res.sendFile("index.html")
-	res.send(__dirname)
+	res.sendFile("index.html",{
+		root: __dirname
+	})
+	//res.send(__dirname)
 })
 
 app.listen(3000)
