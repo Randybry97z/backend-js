d812ce2ec539de794ab38e2e18d48cf0155bdc25
Bryan <bssandovalrod@gmail.com>
2020-05-27 17:05:37 -0500

    Validate user & password

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,54 @@
 'use strict';
+
+const bcrypt = require('bcrypt')
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    password_hash: DataTypes.STRING
+    email: {
+    	type: DataTypes.STRING,
+    	unique: true,
+    	allowNull: false
+   	},
+    password_hash: DataTypes.STRING,
+    password: DataTypes.VIRTUAL
   }, {});
+
+  User.login = function (email,password) {
+  	//Buscar usuario
+  	return User.findOne({
+  		where: {
+  			email
+  		}
+  	}).then(user=>{
+  		if (!user) return null
+  		return user.authenticatePassword(password).then(valid=>{
+  			if (valid ? user : null)
+  		})
+  	})
+  }
+  //Comparar password
+  User.prototype.authenticatePassword = function(password){
+  	return new Promise((res,rej)=>{
+	  	bcrypt.compare(password, this.password_hash, function (err,valid) {
+	  		if (err) return rej(err)
+
+	  		res(valid)
+	  	})
+  	})
+  };
   User.associate = function(models) {
     // associations can be defined here
   };
+  User.beforeCreate(function (user,options) {
+  	return new Promise((res,rej)=>{
+	  	if (user.password) {
+			  bcrypt.hash(user.password, 10, function (error,hash) {
+			  	user.password_hash = hash
+			  	res()
+			  })
+	  	}
+  	})
+
+  })
   return User;
 };
