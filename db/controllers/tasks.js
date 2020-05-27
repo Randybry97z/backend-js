599d0cba5c191e45f3bfc637fd0e72261e182ebe
Bryan <bssandovalrod@gmail.com>
2020-05-27 13:22:39 -0500

    creating records

--- db/controllers/tasks.js
+++ db/controllers/tasks.js
@@ -1,10 +1,23 @@
 const Task = require('../models').Task
 
 module.exports = {
-	home: function (req,res) {
+	create: function (req,res) {
+		Task.create({
+			description: req.body.description
+		}).then(result=>{
+			res.json(result)
+		}).catch(err=>{
+			console.log(err)
+			res.json(err)
+		})
+	},
+	new: function(req,res){
+		res.render('tasks/new')
+	}
+}
+	/*home: function (req,res) {
 		Task.findAll().then(function (tasks) {
 			//Buscar index en carpeta tasks de views
 			res.render('tasks/index', {tasks: tasks})
 		})
-	}
-}
+	}*/
--- db/routes/tasks_routes.js
+++ db/routes/tasks_routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
+let TasksController = require('../controllers/tasks')
 
 let router = express.Router()
 
 router.route('/tasks').get(function (req,res) {
 	res.send('Hola desde subruta')
-}).post(function (req,res) {
+}).post(TasksController.create)
 
-})
+router.get('/tasks/new',TasksController.new)
 
 module.exports = router
