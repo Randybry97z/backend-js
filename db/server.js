a02ff5d368b48de1e9e308b6b222b7945cc2966e
Bryan <bssandovalrod@gmail.com>
2020-05-27 10:43:56 -0500

    Adding sequelize

--- db/server.js
+++ db/server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const sqlite3 = require('sqlite3')
 const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-let db = new sqlite3.Database('proyecto-backend')
+const sequelize = new Sequelize('proyecto-backend',null,null,{
+	dialect: 'sqlite',
+	storage: './proyecto-backend'
+});
+//let db = new sqlite3.Database('proyecto-backend')
 
 app.post('/pendientes', function (req,res) {
-	db.run(`INSERT INTO tasks(description) VALUES('?')`,req.body.description)
+	// db.run(`INSERT INTO tasks(description) VALUES('?')`,req.body.description)
 	res.send("Consulta lista")
 })
 
@@ -16,8 +21,8 @@ app.post('/pendientes', function (req,res) {
 
 app.listen(3000)
 
-process.on('SIGINT',function () {
-	console.log('Adios bby.')
-	db.close()
-	process.exit()
-})
+// process.on('SIGINT',function () {
+// 	console.log('Adios bby.')
+// 	db.close()
+// 	process.exit()
+// })
