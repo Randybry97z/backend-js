7f3f193b96cfe5bca3a40f6da924a486525f40cd
Bryan <bssandovalrod@gmail.com>
2020-05-27 13:44:02 -0500

    Generating view and controller for single task

--- db/controllers/tasks.js
+++ db/controllers/tasks.js
@@ -1,6 +1,16 @@
 const Task = require('../models').Task
 
 module.exports = {
+	index: function (req,res) {
+		Task.findAll().then((tasks)=>{
+			res.render('tasks/index',{tasks: tasks})
+		})
+	},
+	show: function (req,res) {
+		Task.findByPk(req.params.id).then(function (task) {
+			res.render('tasks/show', {task})
+		})
+	},
 	create: function (req,res) {
 		Task.create({
 			description: req.body.description
--- db/routes/tasks_routes.js
+++ db/routes/tasks_routes.js
@@ -3,10 +3,10 @@ let TasksController = require('../controllers/tasks')
 
 let router = express.Router()
 
-router.route('/tasks').get(function (req,res) {
-	res.send('Hola desde subruta')
-}).post(TasksController.create)
+router.route('/tasks').get(TasksController.index).post(TasksController.create)
 
 router.get('/tasks/new',TasksController.new)
+
+router.route('/tasks/:id').get(TasksController.show)
 
 module.exports = router
