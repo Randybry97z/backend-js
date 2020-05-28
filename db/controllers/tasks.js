1c5d39662e779c58e5e5868d6282dee56cc0f860
Bryan <bssandovalrod@gmail.com>
2020-05-28 15:33:44 -0500

    show user tasks

--- db/controllers/tasks.js
+++ db/controllers/tasks.js
@@ -1,13 +1,19 @@
 const Task = require('../models').Task
+const User = require('../models').User
 
 module.exports = {
 	index: function (req,res) {
 		Task.findAll().then((tasks)=>{
-			res.render('tasks/index',{tasks: tasks})
+			res.render('tasks/index',{tasks: req.user.tasks})
 		})
 	},
 	show: function (req,res) {
-		Task.findByPk(req.params.id).then(function (task) {
+		Task.findByPk(req.params.id,{
+			include: [{
+				model: User,
+				as: 'user'
+			}]
+		}).then(function (task) {
 			res.render('tasks/show', {task})
 		})
 	},
@@ -27,7 +33,8 @@ module.exports = {
 	},
 	create: function (req,res) {
 		Task.create({
-			description: req.body.description
+			description: req.body.description,
+			userId: req.user.id
 		}).then(result=>{
 			res.json(result)
 		}).catch(err=>{
